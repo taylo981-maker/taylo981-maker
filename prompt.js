439c3d422134fc8b95b8d3e45ee5a8d2916b7958
taylo981-maker <taylo981@gmail.com>
2026-03-30 21:14:34 -0300

    Salvar aulas feitas hoje

---
+++ prompt.js
@@ -0,0 +1,3 @@
+const prompt = require('prompt-sync')();
+let nome = prompt("Digite seu nome: ");
+console.log('Olá, ' + nome + '!');
--- vetor16.js
+++ vetor16.js
@@ -1,30 +1,20 @@
-let estoquebaixo = [1, 3, 5, 10, 50];
+let estoque = [10, 0 , 5, 1, 15, 2, 8];
+let contardeestoque = 0;
+let estoquebaixo = 0;
+let reposiçaodeestoque = 0;
 
-let estoquealto = [50, 60, 70, 80, 90, 100, 110];
+for (let i = 0; i < estoque.length; i++){
+    let quantidade = estoque[i];
 
-let totalEstoque = 0;
-let semEstoque = 0;
-let baixoEstoque = 0;
-let totalReposicoes = 0;
-
-for (let i = 0; i < estoquebaixo.length; i++){
-    console.log(estoquebaixo[i]);
-    totalEstoque += estoquebaixo[i];
-    if (estoquebaixo[i] === 0){
-        semEstoque++;
-        console.log("Produto sem estoque: " + estoquebaixo[i]);
-    } else if (estoquebaixo[i] < 5){
-        baixoEstoque++;
-        console.log("Estoque baixo: " + estoquebaixo[i]);
-        totalReposicoes += (5 - estoquebaixo[i]);
-    }
+    if (quantidade === 0){
+        contardeestoque++;
+    contardeestoque++;
+    } else if (quantidade < 5){
+        estoquebaixo++;
+    reposiçaodeestoque++;
+    } 
 }
 
-for (let i = 0; i < estoquealto.length; i++){
-    totalEstoque += estoquealto[i];
-}
-
-console.log("Total do estoque: " + totalEstoque);
-console.log("Produtos sem estoque: " + semEstoque);
-console.log("Produtos com estoque baixo: " + baixoEstoque);
-console.log("Total de reposições necessárias: " + totalReposicoes);
+console.log("Produtos sem estoque: " + contardeestoque);
+console.log("Produtos com estoque baixo: " + estoquebaixo);
+console.log("Produtos que precisam de reposição: " + reposiçaodeestoque);
