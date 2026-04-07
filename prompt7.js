e2752ba80a06f00da34b08d86d1fa71eca1443e0
taylo981-maker <taylo981@gmail.com>
2026-04-06 21:08:27 -0300

    dia 06/04 de prompt

--- prompt7.js
+++ prompt7.js
@@ -1,9 +1,58 @@
 const prompt = require("prompt-sync")();
-let opçao = '';
 
-while (opçao !== '3') {
+let opcao = '';
+let alunos = [];
+
+while (opcao !== '4') {
+
     console.log("\n=== Menu ===");
-    console.log("1. usario aluno");
-    console.log("2. Listar Alunos");
-    console.log("3. Sair");
-    opçao = prompt("opção: ");
+    console.log("1. Cadastrar aluno");
+    console.log("2. Listar alunos");
+    console.log("3. Verificar nota");
+    console.log("4. Sair");
+
+    opcao = prompt("Opcao: ");
+
+    if (opcao === '1') {
+
+        const nome = prompt("nome do aluno: ");
+        const idade = prompt("idade do aluno: ");
+        const nota = Number(prompt("nota do aluno: "));
+
+        alunos.push({ nome, idade, nota });
+
+        console.log(`Aluno ${nome} cadastrado com sucesso!`);
+
+    } else if (opcao === '2') {
+
+        console.log("\n=== Lista de alunos ===");
+
+        alunos.forEach((aluno, index) => {
+            console.log(`${index + 1}. ${aluno.nome} - Idade: ${aluno.idade} - Nota: ${aluno.nota}`);
+        });
+
+    } else if (opcao === '3') {
+
+        const nomeBusca = prompt("Digite o nome do aluno: ");
+
+        const aluno = alunos.find(a => a.nome === nomeBusca);
+
+        if (aluno) {
+            if (aluno.nota >= 7) {
+                console.log(`Aprovado! Nota: ${aluno.nota}`);
+            } else {
+                console.log(`Reprovado! Nota: ${aluno.nota}`);
+            }
+        } else {
+            console.log("Aluno não encontrado.");
+        }
+
+    } else if (opcao === '4') {
+
+        console.log("exit...");
+
+    } else {
+
+        console.log("Opção inválida.");
+    }
+}
--- prompt7.js.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-let opçao = '';
-
-while (opçao !== '3') {
-    console.log("\n=== Menu ===");
-    console.log("1. usario aluno");
-    console.log("2. Listar Alunos");
-    console.log("3. Sair");
-    opçao = prompt("opção: ");
